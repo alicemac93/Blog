c84a1f976c5e8d258d3fe2fd8477195bc03f0c26
alicemac93 <alice.maciejewska@gmail.com>
2022-01-18 15:12:51 -0600

    feat: create Nav component from initial Layout

---
+++ src/components/AdminSide/Nav.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import logo from '../../images/logo.svg';
+import styled from 'styled-components';
+
+const ListItem = styled.li`
+    > * {
+        text-decoration: none;
+        color: #000;
+    }
+
+`
+
+const Navbar = styled.ul`
+display: flex;
+flex-orientation: row;
+justify-content: space-between;
+align-items: center;
+list-style: none;
+font-family: Montserrat;
+margin-top: 10px;
+`
+
+const Nav = () => {
+
+    return (
+        <Navbar>
+            <ListItem>
+                <Link to="/"><img alt="logo" src={logo} /></Link>
+            </ListItem>
+            <ListItem>
+                <Link to="/about">About</Link>
+            </ListItem>
+            <ListItem>
+                <Link to="/my-articles">My Articles</Link>
+            </ListItem>
+            <ListItem>
+                <Link to="/createarticle">Create Article</Link>
+            </ListItem>
+        </Navbar>
+    )
+}
+
+export default Nav;
