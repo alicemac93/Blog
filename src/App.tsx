ae5faa78194dd50c9c0cc570503413473e8757ac
alicemac93 <alice.maciejewska@gmail.com>
2022-01-18 15:10:58 -0600

    feat: update app to reflect newly created article components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 
-import AllArticles from './features/admin/allArticles/AllArticles';
-import Nav from './components/Layout';
-import NewArticle from './features/admin/newArticle/NewArticle';
-
+import AllArticles from './features/adminSide/articles/AllArticles';
+import Nav from './components/AdminSide/Layout';
+import NewArticle from './components/AdminSide/NewArticle';
+import EditArticle from "./components/AdminSide/EditArticle";
+import { selectArticles, selectActiveId } from '../src/features/adminSide/articles/ArticlesSlice';
+import { useSelector } from 'react-redux';
 
 function App() {
 
+  const articles = useSelector(selectArticles);
+  const activePostId = useSelector(selectActiveId);
+
   return (
+    <div className="App">
       <Routes>
         <Route path="/" element={<Nav />} >
           <Route path="createarticle" element={<NewArticle />}/>
-          <Route path="articles-recent"/>
           <Route path="about" />
-          <Route path="my-articles" element={<AllArticles />}/>
+          <Route
+            path="my-articles"
+            element={<AllArticles
+              articles={articles}
+              />}
+            />
+          <Route path="editarticle"
+            element={<EditArticle
+              articles={articles}
+              activeId={activePostId}
+              />}
+            />
         </Route>
       </Routes>
+      </div>
   );
 }
 
