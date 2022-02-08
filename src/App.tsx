ca5fb2fe9e4dddb4ee63da18c07780c4c45919e8
alicemac93 <alice.maciejewska@gmail.com>
2022-02-08 13:03:46 -0600

    feat: add new components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import './App.css';
 import AllArticles from './features/adminSide/articles/AllArticles';
-import Nav from './components/AdminSide/Layout';
+import Nav from './Layout';
 import NewArticle from './components/AdminSide/NewArticle';
 import EditArticle from "./components/AdminSide/EditArticle";
 import Inspiration from "./components/AdminSide/Inspiration";
 import Home from "./components/AdminSide/Home";
-
-import { selectArticles, selectActiveId } from '../src/features/adminSide/articles/ArticlesSlice';
+import Login from "./components/login";
+import RecentArticles from "./components/UserSide/RecentArticles";
+import ArticleDetail from "./components/UserSide/ArticleDetail";
+import { selectArticles, selectActiveId, selectActiveArticleId } from '../src/features/adminSide/articles/ArticlesSlice';
 import { useSelector } from 'react-redux';
 
 function App() {
 
   const articles = useSelector(selectArticles);
   const activePostId = useSelector(selectActiveId);
+  const activeArticleId = useSelector(selectActiveArticleId)
+  const [token, setToken] = useState(); /* https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications */
 
   return (
     <div className="App">
       <Routes>
         <Route
               path="/"
-              element={<Nav />} >
+              element={<Nav
+                isLoggedIn/>} >
+          <Route
+              path={`article-detail/${activeArticleId}`}
+              element={<ArticleDetail
+                articles={articles}
+                activeArticleId={activeArticleId} />}/>
+          <Route
+              path="recent-articles"
+              element={<RecentArticles
+                articles={articles} />}/>
           <Route
               path="createarticle"
               element={<NewArticle />}/>
@@ -30,19 +45,22 @@ function App() {
           <Route
               path="my-articles"
               element={<AllArticles
-              articles={articles}
+                articles={articles}
               />}
             />
           <Route
               path="editarticle"
               element={<EditArticle
-              articles={articles}
-              activeId={activePostId}
+                articles={articles}
+                activeId={activePostId}
               />}
             />
             <Route
               path="home"
               element={<Home />} />
+            <Route
+              path="login"
+              element={<Login />} />
         </Route>
       </Routes>
       </div>
