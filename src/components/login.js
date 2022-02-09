ab7a79f1f4ca737f91d0e43d7d7c32044a9d19b6
alicemac93 <alice.maciejewska@gmail.com>
2022-02-08 21:33:47 -0600

    feat: update login component

--- src/components/login.js
+++ src/components/login.js
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { useDispatch } from "react-redux"
 import { setActiveUser } from "../features/adminSide/users/UsersSlice";
 import { useNavigate } from "react-router-dom";
+import { PublishButton } from "../styled-componets";
 
 export default function Login() {
   const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -15,14 +17,16 @@ export default function Login() {
   }
 
   return (
-    <form>
-      <label>
-        <p>Username</p>
-        <input type="text" onChange={(e) => setUser(e.target.value)} />
-      </label>
-      <div>
-        <button type="submit" onClick={(e) => handleLogin(e)}>Submit</button>
-      </div>
-    </form>
+    <div className="login-wrapper">
+      <form className="column">
+        <label for="userName">Username</label>
+        <input id="userName" name="userName" type="text" onChange={(e) => setUser(e.target.value)} />
+        <label for="password">Password</label>
+        <input id="password" name="password" type="text" onChange={(e) => setPassword(e.target.value)} />
+        <PublishButton type="submit" onClick={(e) => handleLogin(e)}>
+          Login
+        </PublishButton>
+      </form>
+    </div>
   )
 }
